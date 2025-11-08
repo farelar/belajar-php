f2702b8e4b03e1d4b356ca631301cf5e5143a9bc
farelar <arriziqfarel@gmail.com>
2025-11-08 20:13:02 +0700

    tambah file

---
+++ ForLoop.php
@@ -0,0 +1,14 @@
+<?php
+
+$counter = 1;
+// tanpa henti
+// for( ; ;){
+//     echo "this is for loop" . PHP_EOL;
+// }
+
+for( $counter = 1; $counter <= 10; $counter++){
+    echo "Counter ke " . $counter . PHP_EOL;
+}
+for( $counter = 10; $counter >= 1; $counter--){
+    echo "Counter ke " . $counter . PHP_EOL;
+}
---
+++ WhileLoop.php
@@ -0,0 +1,8 @@
+<?php
+// while lebih sederhana dari for loop karena hanya memiliki kondisi saja
+$counter = 2;
+
+while( $counter <= 10 ){
+    echo "While Loop ke " . $counter . PHP_EOL;
+    $counter++;
+}
